c1b390e4bb3b2afefb3c2abf475c2f0d7eff2016
/dev/null <dev_per_null@hotmail.com>
2018-07-29 23:23:42 +0200

    Initial commit (webpack proofe of concept)

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/template.index.html',
+            title: 'myInfo',
+            filename: 'index.html',
+            excludeChunks: [ 'print' ]
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template.test.html',
+            title: 'myInfo TEST PAGE',
+            filename: 'test.html'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
